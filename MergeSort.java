fcc6df49a8906b608c72232f25b0844d1182adc1
Symbat Dulatuly <synbat1999@gmail.com>
2022-08-31 02:46:22 +0600

    mergeSort added

--- MergeSort.java
+++ MergeSort.java
@@ -1,26 +1,47 @@
 
 public class MergeSort {
-	public static int [] merge (int [] a, int [] b){
-		int alength = a.length;
-		int blength = b.length;
+	public static void merge (int [] arr, int l, int m, int h) {
+		int n1 = m-l+1;
+		int n2 = h-m;
+		int [] arr1 = new int[n1];
+		int [] arr2 = new int[n2];
+		for (int i=0; i<n1; ++i) {
+			arr1[i] = arr[l+i];
+		}
+		for (int i=0; i<n2; ++i) {
+			arr2[i] = arr[m+i+1];
+		}
+		
 		int i=0;
 		int j=0;
-		int k=0;
-		int [] c = new int[alength+blength];
-		while (i<alength && j<blength) {
-			if (a[i]<b[j]) {
-				c[k++] = a[i++];
+		int k=l;
+		
+		while (i < n1 && j < n2) {
+			if (arr1[i]<arr2[j]) {
+				arr[k] = arr1[i];
+				i++;
 			}
 			else {
-				c[k++] = b[j++];
+				arr[k] = arr2[j];
+				j++;
 			}
+			k++;
+		} 
+		while (i<n1) {
+			arr[k++] = arr1[i++];
 		}
-		for (; i<alength; i++){
-			c[k++] = a[i++];
+		while (j<n2) {
+			arr[k++] = arr2[j++];
 		}
-		for (; j<blength; j++){
-			c[k++] = b[j++];
+		
+	}
+	
+	static void sort (int [] arr, int l, int h) {
+		if (l<h) {
+			int m = (l+h)/2;
+			sort(arr, l, m);
+			sort(arr, m+1, h);
+			merge(arr, l, m, h);
 		}
-		return c;
 	}
 }
--- Test.java
+++ Test.java
@@ -1,19 +1,9 @@
 public class Test {
 	public static void main(String [] args) {
-		int [] a = {1,3,5,7,8, 25, 35};
-		int [] b = {2,6,9,11,15, 17, 20};
-		int [] c = MergeSort.merge(a,b);
+		int [] a = {10, 15,1, 3,4 , 2, 0};
+		MergeSort.sort(a, 0, 6);
 		for (int i=0; i<a.length; i++) {
 			System.out.print(a[i]+ " ");
 		}
-		System.out.println();
-		for (int i=0; i<b.length; i++) {
-			System.out.print(b[i] + " ");
-		}
-		System.out.println();
-		for (int i=0; i<c.length; i++) {
-			System.out.print(c[i]+ " ");
-		}
-		System.out.println();
 	}
 }
